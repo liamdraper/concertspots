cfa5bec6d00193cba6477e3fecf2fe5551c977fc
liamdraper <liamdraper83@gmail.com>
2023-03-04 08:45:22 -0800

    Re-style navbar with bootstrap and implement API call in views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('tickets/', views.tickets_index, name='index'),
-    path('tickets/create/', views.TicketCreate.as_view(), name='tickets_create')
+    path('tickets/create/', views.TicketCreate.as_view(), name='tickets_create'),
+    path('concerts/', views.concerts, name='concerts')
 ]
