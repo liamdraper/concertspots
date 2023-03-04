3dbe22c9839429f3fadf04f1ee1c13b08c5d8311
liamdraper <liamdraper83@gmail.com>
2023-03-03 21:15:08 -0800

    Nav bar and basic ticket functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+class Ticket(models.Model):
+    price = models.IntegerField()
+    seat = models.CharField(max_length=5)
+    number = models.IntegerField()
+
+    def get_absolute_url(self):
+        return reverse('index')
