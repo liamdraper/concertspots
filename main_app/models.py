e60de0a4c29fcf5b974d5b68414b6899ac2737eb
liamdraper <liamdraper83@gmail.com>
2023-03-06 15:43:02 -0800

    Add to search functionality

--- main_app/models.py
+++ main_app/models.py
@@ -4,14 +4,23 @@ from django.urls import reverse
 from datetime import datetime
 # Create your models here.
 
+class Concert(models.Model):
+    event_name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField(default=datetime.now)
+    api_id = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.event_name} ({self.id})'
+
 class Ticket(models.Model):
     event_name =  models.CharField(max_length=200)
     price = models.IntegerField()
     location = models.CharField(max_length=200)
     date = models.DateTimeField(default=datetime.now)
 
-    def get_absolute_url(self):
-        return reverse('index')
-    
-    def get_absolute_url(self):
-        return reverse('ticket_detail', kwargs={'pk': self.id})
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.event_name} ({self.id})'
