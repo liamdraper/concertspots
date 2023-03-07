84518217e4e3c61e81a2e1f52ce1394f996842fd
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-03-07 12:01:00 -0800

    Added styling to most pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,9 +11,8 @@ urlpatterns = [
     path('tickets/<int:ticket_id>', views.ticket_detail, name='ticket_detail'),
     path('tickets/<int:pk>/delete/', views.TicketDelete.as_view(), name='ticket_delete'),
     path('tickets/create/', views.add_ticket, name='ticket_create'),
-    path('concerts/', views.concerts, name='concerts'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout')
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('concerts/', views.concerts_search, name='search'),
     path('concerts/index', views.concerts_index, name='concerts_index'),
     path('concerts/<int:concert_id>', views.concert_detail, name='concert_detail'),
