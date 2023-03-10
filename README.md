e2672bc00401e7aacc924461beb45d061647c8d2
liamdraper <liamdraper83@gmail.com>
2023-03-10 08:13:29 -0800

    Final

--- README.md
+++ README.md
@@ -8,9 +8,14 @@ concertSpots is a web application for finding concerts and buying tickets.
 
 - Browse to [https://concertspots.herokuapp.com/](https://concertspots.herokuapp.com/)
 - [Trello Planning Board](https://trello.com/b/hFeflKMZ/project-3-concertspots3)
+- [Pitchdeck Presentation](https://docs.google.com/presentation/d/1xjnAAMj6frpPT2KCuugUSBKg3-0sQbqT31wN41p-EgY/edit#slide=id.g21223fea3d1_0_25)
 - After opening to the landing page, create an account to start finding and buying tickets
 
 ## Screenshots
+
+![alt text](/photos/screenshot1.jpg)
+![alt text](/photos/screenshot2.jpg)
+![alt text](/photos/screenshot3.jpg)
 
 ## Built with
 - Python
@@ -28,4 +33,4 @@ concertSpots is a web application for finding concerts and buying tickets.
 - Zakariye Ahmead (Developer)
 
 ## Next Steps:
-- 
+- Implement seat picker functionality for each ticket
--- main_app/views.py
+++ main_app/views.py
@@ -5,45 +5,53 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import Ticket, Concert
 import requests
 from django.contrib.auth import logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 client_id = 'client_id=MzIxMjg4OTl8MTY3Nzk0NjYzNi4zMDA4MDYz'
 
-# Create your views here.
+@login_required
 def home(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     return render(request, 'home.html', {
         'cart_count': cart_count
     })
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('home')
 
+@login_required
 def about(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     return render(request, 'about.html', {
         'cart_count': cart_count
     })
 
+@login_required
 def tickets_index(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
-    tickets = Ticket.objects.all().filter(bought=True)
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
+    tickets = Ticket.objects.all().filter(bought=True, user=request.user)
     return render(request, 'tickets/index.html', {
         'tickets': tickets, 'cart_count': cart_count
     })
 
+@login_required
 def ticket_detail(request, ticket_id):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     ticket = Ticket.objects.get(id=ticket_id)
     return render(request, 'tickets/detail.html', {
         'ticket': ticket, 'cart_count': cart_count
     })
-    
+
 def login_view(request):
     form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
 def logout_view(request):
     logout(request)
     return redirect('home')
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -58,20 +66,21 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
-class TicketDelete(DeleteView):
+class TicketDelete(LoginRequiredMixin, DeleteView):
     model = Ticket
     success_url = '/tickets'
 
+@login_required
 def concerts_index(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     concerts = Concert.objects.all()
     return render(request, 'concerts/concerts_index.html', {
         'concerts': concerts, 'cart_count': cart_count
     })
 
-# The user search input is not getting saved because the function gets reset each time, and the variable "search" isn't saved
+@login_required
 def concerts_search(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     concerts = []
     search = request.POST.get('name')
     response = requests.get(f'https://api.seatgeek.com/2/events?q={search}&{client_id}').json()
@@ -102,12 +111,9 @@ def concerts_search(request):
         'concerts': concerts, 'search': search, 'cart_count': cart_count
     })
 
-def sort_searches(request):
-    pass
-
-
+@login_required
 def concert_detail(request, concert_id):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     concert = Concert.objects.filter(api_id=concert_id).first()
     price = 100
     if request.POST.get('price') == None:
@@ -120,7 +126,7 @@ def concert_detail(request, concert_id):
             date = request.POST.get('date'),
             api_id = request.POST.get('api_id')
         )
-    if request.POST:
+    if request.POST.get('add') == 'add':
         new_ticket = Ticket(
             event_name = concert.event_name,
             price = concert.price,
@@ -128,26 +134,38 @@ def concert_detail(request, concert_id):
             date = concert.date
         )
         new_ticket.concert_id = concert.id
-        new_ticket.save()   
+        new_ticket.user = request.user
+        new_ticket.save()
+        return redirect('ticket_index')   
     return render(request, 'concerts/detail.html', {
         'concert': concert, 'cart_count': cart_count
     })
 
+@login_required
 def nav_cart(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
     return render(request, 'base.html', {
         'cart_count': cart_count
     })
 
+@login_required
 def cart(request):
-    cart_count = Ticket.objects.filter(bought=False).count()
-    items = Ticket.objects.filter(bought=False)
+    cart_count = Ticket.objects.filter(bought=False, user=request.user).count()
+    items = Ticket.objects.filter(bought=False, user=request.user)
+    item_id = request.POST.get('delete')
+    if request.POST:
+        item = Ticket.objects.filter(id=item_id)
+        item.delete()
+    total = 0
+    for item in items:
+        total += item.price
     return render(request, 'checkout/cart.html', {
-        'items': items, 'cart_count': cart_count
+        'items': items, 'cart_count': cart_count, 'total': total
     })
 
+@login_required
 def checkout(request):
-    items = Ticket.objects.filter(bought=False)
+    items = Ticket.objects.filter(bought=False, user=request.user)
     if request.POST.get('empty') == 'Empty Cart':
         for item in items:
             item.delete()
